1d25ca894f89f0f579500cf5ca12a927eea210d5
Matti Jokitulppo <melonmanchan@gmail.com>
2018-08-13 16:27:49 +0300

    Refactor secret stuff under .env

--- build_all.sh
+++ build_all.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Building publishing app"
-docker build ~/projects/dippa/dippa-publish -t publish
+#echo "Building publishing app"
+#docker build ~/projects/dippa/dippa-publish -t publish
+#
+#echo "Building google facial recognition"
+#docker build ~/projects/dippa/dippa-facerec -t google_image
+#
+#echo "Building IBM text recognition"
+#docker build ~/projects/dippa/dippa-nlp -t ibm_text
+#
 
-echo "Building google facial recognition"
-docker build ~/projects/dippa/dippa-facerec -t google_image
-
-echo "Building IBM text recognition"
-docker build ~/projects/dippa/dippa-nlp -t ibm_text
+echo "Building writer"
+docker build ~/projects/dippa/dippa-writer -t writer
