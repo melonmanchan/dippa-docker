c95634bc3d1c7128c02e89dc2f53d00d7543160b
Matti Jokitulppo <melonmanchan@gmail.com>
2018-08-27 15:45:22 +0300

    Add writer support

--- build_all.sh
+++ build_all.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
-#echo "Building publishing app"
-#docker build ~/projects/dippa/dippa-publish -t publish
-#
-#echo "Building google facial recognition"
-#docker build ~/projects/dippa/dippa-facerec -t google_image
-#
-#echo "Building IBM text recognition"
-#docker build ~/projects/dippa/dippa-nlp -t ibm_text
-#
+echo "Building publishing app"
+docker build ~/projects/dippa/dippa-publish -t publish
+
+echo "Building google facial recognition"
+docker build ~/projects/dippa/dippa-facerec -t google_image
+
+echo "Building IBM text recognition"
+docker build ~/projects/dippa/dippa-nlp -t ibm_text
 
 echo "Building writer"
 docker build ~/projects/dippa/dippa-writer -t writer
