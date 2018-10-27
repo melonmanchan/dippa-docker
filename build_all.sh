adda2a84135e2867de30a1a25a51a47be19aa213
Matti Jokitulppo <melonmanchan@gmail.com>
2018-10-27 16:56:59 +0300

    Update build script

--- build_all.sh
+++ build_all.sh
@@ -2,13 +2,19 @@
 set -e
 
 echo "Building publishing app"
-docker build ~/projects/dippa/dippa-publish -t publish
+docker build --no-cache ~/projects/dippa/dippa-publish -t publish
 
 echo "Building google facial recognition"
-docker build ~/projects/dippa/dippa-facerec -t google_image
+docker build --no-cache ~/projects/dippa/dippa-facerec -t google_image
 
 echo "Building IBM text recognition"
-docker build ~/projects/dippa/dippa-nlp -t ibm_text
+docker build --no-cache ~/projects/dippa/dippa-nlp -t ibm_text
 
 echo "Building writer"
-docker build ~/projects/dippa/dippa-writer -t writer
+docker build --no-cache ~/projects/dippa/dippa-writer -t writer
+
+echo "Building front"
+docker build --no-cache ~/projects/dippa/dippa-front -t front
+
+echo "Building vis"
+docker build --no-cache ~/projects/dippa/dippa-vis -t vis
